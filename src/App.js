954376e7f103e0b8a9251892c0ffb9e42799c56c
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-07-05 15:46:30 +0300

    data ve basit bir sayfa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [nasaData, setNasaData] = useState({});
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=7La2DSBifCI4RGuacTKdaSMoTuiPVRIeAbmEFHE3"
+      )
+      .then((response) => setNasaData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="App">
+      <h1>{nasaData.title}</h1>
+      <img src={nasaData.url} alt="Günün fotoğrafı" />
+      <h3>{nasaData.date}</h3>
+      <h4>{nasaData.copyright}</h4>
       <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
+        <span role="img" aria-label="go!">
+          🚀🚀🚀🚀🚀
+          <br></br>
+          <br></br>
+        </span>
+
+        {nasaData.explanation}
       </p>
     </div>
   );
